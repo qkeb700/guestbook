389b845a350d27c3199b55f15517c9d044ae3f14
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-02-06 01:17:43 +0900

    feat : 로그인하면 게시물 삭제 기능, API 추가
    로그인이 이루어지면 게시물을 삭제할 수 있게된다. 
    API 로 데이터를 읽을 수 있다.

--- src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -3,15 +3,20 @@ package kr.or.connect.guestbook.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.or.connect.guestbook.dto.Guestbook;
 import kr.or.connect.guestbook.service.GuestbookService;
@@ -23,7 +28,21 @@ public class GuestbookController {
 	
 	@GetMapping(path="/list")
 	public String list(@RequestParam(name="start", required=false, defaultValue="0") int start, 
-			ModelMap model) {
+			ModelMap model, @CookieValue(value="count", defaultValue="0", required=true)String value, HttpServletResponse response) {
+		
+		 
+			try {
+				int i = Integer.parseInt(value);
+				value = Integer.toString(++i);
+			}catch(Exception e) {
+				value = "1";
+			}
+		
+		
+		Cookie cookie = new Cookie("count", value);
+		cookie.setMaxAge(60*30);
+		cookie.setPath("/");
+		response.addCookie(cookie);
 		
 		// start로 시작하는 방명록 목록 구하기
 		List<Guestbook> list = guestbookService.getGuestbooks(start);
@@ -47,6 +66,7 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		model.addAttribute("count", count);
 		model.addAttribute("pageStartList", pageStartList);
+		model.addAttribute("cookieCount", value);
 		
 		return "list";
 	}
@@ -59,6 +79,20 @@ public class GuestbookController {
 		guestbookService.addGuestbook(guestbook, clientIp);
 		return "redirect:list";
 	}
-	
+	@GetMapping("/delete")
+	public String delete(@RequestParam(name="id", required=true)Long id,
+			@SessionAttribute("isAdmin") String isAdmin,
+			HttpServletRequest request,
+			RedirectAttributes redirectdAttr) {
+		
+		if(isAdmin == null || !"true".equals(isAdmin)) {
+			redirectdAttr.addFlashAttribute("errorMessage", "로그인을 하지않았습니다.");
+			return "redirect:/loginform";
+		}
+		String clientIp = request.getRemoteAddr();
+		guestbookService.deleteGuestbook(id, clientIp);
+		
+		return "redirect:/list";
+	}
 	
 }
