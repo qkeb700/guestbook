7d912bd3c415e1cfb02a74e8e0719a4ec297f7bc
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2022-02-08 01:42:41 +0900

    feat : headerInfo 출력
    아규먼트리졸버를 통해 헤더에 있는 값을 리스트에 보이기.

--- src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import kr.or.connect.guestbook.argumentresolver.HeaderInfo;
 import kr.or.connect.guestbook.dto.Guestbook;
 import kr.or.connect.guestbook.service.GuestbookService;
 
@@ -28,7 +29,13 @@ public class GuestbookController {
 	
 	@GetMapping(path="/list")
 	public String list(@RequestParam(name="start", required=false, defaultValue="0") int start, 
-			ModelMap model, @CookieValue(value="count", defaultValue="0", required=true)String value, HttpServletResponse response) {
+			ModelMap model, @CookieValue(value="count", defaultValue="0", required=true)String value, HttpServletResponse response,
+			HeaderInfo headerInfo) {
+		
+		
+		System.out.println("-----------------------------------------------------");
+		System.out.println(headerInfo.get("user-agent"));
+		System.out.println("-----------------------------------------------------");
 		
 		 	// 받아온 쿠키값을 기본값으로 값이 1씩 증가한다
 			try {
